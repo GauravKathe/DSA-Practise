49f8eda6e5ad94850dfc4a7c9646daad038370b2
Gaurav <gauravkatheoff@gmail.com>
2025-04-08 00:37:06 +0530

    3 files strings

---
+++ 1614-Maximum nesting parenthesis inside/parenthesis.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxDepth(String s) {
+        int c=0;
+        int max=0;
+        for(int i=0;i<s.length();i++){
+
+            if(s.charAt(i)=='('){
+                c++;
+                max=Math.max(max,c);
+            }
+            else if(s.charAt(i)==')'){
+                c--;
+            }
+        }
+        return max;
+    }
+}
